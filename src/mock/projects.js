561d6b3928dd99f7bb21f6277e8bed43a6514fe7
ilrosch <59929672+ilrosch@users.noreply.github.com>
2022-12-20 17:22:05 +0300

    Page projects

---
+++ src/mock/projects.js
@@ -0,0 +1,69 @@
+export const projects = [
+    {
+        id: "00000",
+        name: "Название проекта #1",
+        author: "Иван Иванов",
+        date: "12.12.2020",
+        description: "Описание проекта. Lorem ipsum dolor sit amet consectetur. Amet nibh sociis porta consectetur odio nisi iaculis pretium. Euismod est metus vulputate vel massa ultrices aliquam. Diam vitae lobortis hendrerit urna. Ligula sed eget scelerisque urna ac condimentum porttitor. Porttitor sed orci eleifend nulla in. Tempor arcu fermentum sed amet diam pellentesque nulla. Faucibus leo morbi eget urna tortor sed. Integer maecenas venenatis id est nam adipiscing sollicitudin malesuada vel.",
+        skills: [
+            "HTML", "CSS", "JavaScript", "React"
+        ],
+        socials: {
+            github: "https://github.com/",
+            telegram: "https://t.me/"
+        },
+        demo: "https://demo.ru/",
+        image: "https://demo.ru/image.jpg"
+    },
+
+    {
+        id: "00001",
+        name: "Название проекта #1",
+        author: "Иван Иванов",
+        date: "12.12.2020",
+        description: "Описание проекта. Lorem ipsum dolor sit amet consectetur. Amet nibh sociis porta consectetur odio nisi iaculis pretium. Euismod est metus vulputate vel massa ultrices aliquam. Diam vitae lobortis hendrerit urna. Ligula sed eget scelerisque urna ac condimentum porttitor. Porttitor sed orci eleifend nulla in. Tempor arcu fermentum sed amet diam pellentesque nulla. Faucibus leo morbi eget urna tortor sed. Integer maecenas venenatis id est nam adipiscing sollicitudin malesuada vel.",
+        skills: [
+            "HTML", "CSS", "JavaScript", "React"
+        ],
+        socials: {
+            github: "https://github.com/",
+            telegram: "https://t.me/"
+        },
+        demo: "https://demo.ru/",
+        image: "https://demo.ru/image.jpg"
+    },
+
+    {
+        id: "00002",
+        name: "Название проекта #1",
+        author: "Иван Иванов",
+        date: "12.12.2020",
+        description: "Описание проекта. Lorem ipsum dolor sit amet consectetur. Amet nibh sociis porta consectetur odio nisi iaculis pretium. Euismod est metus vulputate vel massa ultrices aliquam. Diam vitae lobortis hendrerit urna. Ligula sed eget scelerisque urna ac condimentum porttitor. Porttitor sed orci eleifend nulla in. Tempor arcu fermentum sed amet diam pellentesque nulla. Faucibus leo morbi eget urna tortor sed. Integer maecenas venenatis id est nam adipiscing sollicitudin malesuada vel.",
+        skills: [
+            "HTML", "CSS", "JavaScript", "React"
+        ],
+        socials: {
+            github: "https://github.com/",
+            telegram: "https://t.me/"
+        },
+        demo: "https://demo.ru/",
+        image: "https://demo.ru/image.jpg"
+    },
+
+    {
+        id: "00003",
+        name: "Название проекта #1",
+        author: "Иван Иванов",
+        date: "12.12.2020",
+        description: "Описание проекта. Lorem ipsum dolor sit amet consectetur. Amet nibh sociis porta consectetur odio nisi iaculis pretium. Euismod est metus vulputate vel massa ultrices aliquam. Diam vitae lobortis hendrerit urna. Ligula sed eget scelerisque urna ac condimentum porttitor. Porttitor sed orci eleifend nulla in. Tempor arcu fermentum sed amet diam pellentesque nulla. Faucibus leo morbi eget urna tortor sed. Integer maecenas venenatis id est nam adipiscing sollicitudin malesuada vel.",
+        skills: [
+            "HTML", "CSS", "JavaScript", "React"
+        ],
+        socials: {
+            github: "https://github.com/",
+            telegram: "https://t.me/"
+        },
+        demo: "https://demo.ru/",
+        image: "https://demo.ru/image.jpg"
+    },
+];
