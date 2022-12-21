130c330f1c02ade334fde0c07d8d5ebd427d8f30
ilrosch <59929672+ilrosch@users.noreply.github.com>
2022-12-21 17:17:44 +0300

    HomeSection and smaller container

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,15 +2,22 @@ import { YMaps } from "@pbe/react-yandex-maps";
 import MapWithFavPlaces from "../../components/MapWithFavPlaces";
 import SwiperSlider from "../../components/SwiperSlider/SwiperSlider";
 import Feedback from "../../components/Feedback/Feedback";
+import { HomeSection } from "../../components/HomeSection/HomeSection";
 
 export default function Home() {
   return (
     <>
-      <SwiperSlider />
-      <YMaps>
-        <MapWithFavPlaces />
-      </YMaps>
-      <Feedback />
+      <HomeSection />
+      <div className="container-slider">
+        <SwiperSlider />
+      </div>
+
+      <div className="container">
+        <YMaps>
+          <MapWithFavPlaces />
+        </YMaps>
+        <Feedback />
+      </div>
     </>
   );
 }
