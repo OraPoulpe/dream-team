ebcded97220bfabda640d431a95f7590d7d5812a
SkyGuy <nurmagomedoffzaur@yandex.ru>
2022-12-22 17:27:42 +0300

    add translate to projects

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useContext } from "react";
 import classNames from "classnames";
+import { useTranslation } from "react-i18next";
 
 import { ThemeContext } from "../../context/ThemeProvider";
 import github from "../../img/icons/github-white.svg";
@@ -8,17 +9,18 @@ import Titles from "../../components/Titles/Titles";
 import projects from "../../mock/projects";
 import "./Projects.scss";
 
-
 export default function Projects() {
   const { theme } = useContext(ThemeContext);
+  const { t } = useTranslation();
+
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [])
+  }, []);
 
   return (
     <section className={classNames("projects", { dark: theme === "dark" })}>
       <Titles
-        title="Наши проекты"
+        title={t("Наши проекты")}
         subtitle="Lorem ipsum dolor sit amet consectetur. Porta cursus qu+is blandit orci leo."
       />
       <div className="projects__container">
@@ -26,29 +28,29 @@ export default function Projects() {
           projects.map((project) => (
             <div className="projects__cards">
               <div className="projects__card">
-                <img src={project.image} alt="project" className="image" />
+                <img src={project.image} alt="project" className="Фото" />
                 <div className="projects__shadow" />
 
                 <div className="projects__header">
-                  <h2 className="projects__title">{project.name}</h2>
+                  <h2 className="projects__title">{t(project.name)}</h2>
                   <div className="projects__subtitle">
-                    <h3>{project.author}</h3>
-                    <h3>{project.date}</h3>
+                    <h3>{t(project.author)}</h3>
+                    <h3>{t(project.date)}</h3>
                   </div>
                 </div>
               </div>
               <div className="projects__hover">
-                <h2 className="projects__titles">{project.name}</h2>
+                <h2 className="projects__titles">{t(project.name)}</h2>
                 <div className="projects__title-block">
                   <h3 className="projects__subtitles">
-                    Автор проекта: <span>{project.author}</span>
+                    {t("Автор проекта")}: <span>{t(project.author)}</span>
                   </h3>
                   <h3 className="projects__subtitles">
-                    Дата создания проекта: <span className="date">{project.date}</span>
+                    {t("Дата создания проекта")}: <span className="date">{project.date}</span>
                   </h3>
                 </div>
 
-                <p className="projects__description">{project.description}</p>
+                <p className="projects__description">{t(project.description)}</p>
 
                 <div className="projects__skills">
                   {project.skills.map((skill) => (
@@ -60,7 +62,7 @@ export default function Projects() {
 
                 <div className="projects__contact">
                   <a href={project.demo}>
-                    <button type="button">Посмотреть проект</button>
+                    <button type="button">{t("Посмотреть проект")}</button>
                   </a>
 
                   <div className="projects__icons">
